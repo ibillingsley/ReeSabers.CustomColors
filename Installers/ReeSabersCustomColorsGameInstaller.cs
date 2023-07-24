7d57e7e89fc7b20e88376f26e0b6b8256040d03a
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2023-07-23 23:23:42 -0400

    Add project files.

---
+++ Installers/ReeSabersCustomColorsGameInstaller.cs
@@ -0,0 +1,13 @@
+﻿using ReeSabers.CustomColors.AffinityPatches;
+using Zenject;
+
+namespace ReeSabers.CustomColors.Installers
+{
+	internal class ReeSabersCustomColorsGameInstaller : Installer
+	{
+		public override void InstallBindings()
+		{
+			Container.BindInterfacesTo<ReeSabersPatches>().AsSingle();
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,35 @@
+﻿using IPA;
+using ReeSabers.CustomColors.Installers;
+using SiraUtil.Zenject;
+using IPALogger = IPA.Logging.Logger;
+
+namespace ReeSabers.CustomColors
+{
+	[Plugin(RuntimeOptions.SingleStartInit)]
+	public class Plugin
+	{
+		internal static Plugin Instance { get; private set; }
+		internal static IPALogger Log { get; private set; }
+
+		[Init]
+		public Plugin(IPALogger logger, Zenjector zenjector)
+		{
+			Instance = this;
+			Log = logger;
+			zenjector.Install<ReeSabersCustomColorsGameInstaller>(Location.Player);
+		}
+
+		[OnStart]
+		public void OnApplicationStart()
+		{
+			Plugin.Log.Info("OnApplicationStart");
+		}
+
+		[OnExit]
+		public void OnApplicationQuit()
+		{
+
+		}
+
+	}
+}
