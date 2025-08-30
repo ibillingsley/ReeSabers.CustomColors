9e40a6f2a729fe075fa4b110d0269541ec33c5b2
Isaiah Billingsley <iza.billingsley@gmail.com>
2025-08-30 17:45:25 -0400

    New patching method, remove Publicize

--- AffinityPatches/ReeSabersPatches.cs
+++ AffinityPatches/ReeSabersPatches.cs
@@ -1,22 +1,15 @@
 ﻿using SiraUtil.Affinity;
-using UnityEngine;
 
 namespace ReeSabers.CustomColors.AffinityPatches
 {
 	public class ReeSabersPatches : IAffinity
 	{
 		[AffinityPrefix]
-		[AffinityPatch(typeof(ColorController), nameof(ColorController.Update))]
-		public bool ColorControllerPatch(ColorController __instance)
+		[AffinityPatch(typeof(HsbTransform), "HsbTransform", AffinityMethodType.Constructor, null, typeof(ColorTransformType), typeof(float), typeof(float), typeof(float), typeof(float))]
+		public void HsbTransformPatch(ref ColorTransformType type)
 		{
-			if (__instance._isDirty && __instance._settings.type == ColorTransformType.NotesColor)
-			{
-				Color.RGBToHSV(ColorController.GetNotesColor(__instance._saberType, __instance._settings.colorSource), out var H, out var S, out var V);
-				__instance.HsbTransform.SetValue(new HsbTransform(ColorTransformType.HueShift, H, S, V, __instance._settings.fakeGlowMultiplier), __instance);
-				__instance._isDirty = false;
-				return false;
-			}
-			return true;
+			if (type == ColorTransformType.NotesColor)
+				type = ColorTransformType.HueShift;
 		}
 	}
 }
